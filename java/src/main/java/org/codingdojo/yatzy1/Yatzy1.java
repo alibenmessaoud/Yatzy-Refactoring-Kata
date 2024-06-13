8ef76b1059157f8690e4a5923a42d9e37e18f0e3
Your Name <you@example.com>
2024-06-13 06:54:50 +0200

    format code and rename variables/methods

--- java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
+++ java/src/main/java/org/codingdojo/yatzy1/Yatzy1.java
@@ -6,14 +6,14 @@ public class Yatzy1 {
     public Yatzy1() {
     }
 
-    public Yatzy1(int d1, int d2, int d3, int d4, int _5) {
+    public Yatzy1(int d1, int d2, int d3, int d4, int d5) {
         this();
         dice = new int[5];
         dice[0] = d1;
         dice[1] = d2;
         dice[2] = d3;
         dice[3] = d4;
-        dice[4] = _5;
+        dice[4] = d5;
     }
 
     public static int chance(int d1, int d2, int d3, int d4, int d5) {
